034153962269128843c258f566bff75630f8ced9
Tyler Erickson <tyler.j.erickson@seagate.com>
2018-08-22 10:21:40 -0600

    Adding a couple updates to the WinAPI versions

--- include/common_windows.h
+++ include/common_windows.h
@@ -113,19 +113,22 @@ extern "C"
 #warning WARNING: WIN_API_TARGET_VERSION is not set.This should be set to a value in common_windows.h. If unsure, define it as 81000000L
 #endif
 #endif
-
+    //SDK Versions can be found here: https://en.wikipedia.org/wiki/Microsoft_Windows_SDK
+    //Add more below as we need them
     //These defines were added for comparison so you know which Windows API is being targetted.
     //The preprocessor should define WIN_API_TARGET_VERSION as one of these values so that it is known which API version is being targetted for build.
     //This is not available through including header files as far as I know - TJE
     //Not defining this may cause some compiler errors as different calls will be available to different API versions...same with different API flags to some functions.
     #define WIN_API_TARGET_UNKNOWN 0 //Unknown target, or not defined by builder
-    #define WIN_API_TARGET_81_AND_OLDER 81000000L //8.1.00000.0 //In VS2013 & VS2015, you select the Windows 8.1 API as the lowest target. VS2013 doesn't offer an API higher than this.
-    #define WIN_API_TARGET_WIN10_10240 100102400L //10.0.10240.0 //Win 10 API, build 10240
-    #define WIN_API_TARGET_WIN10_10586 100105860L //10.0.10586.0 //Win 10 API, build 10586
-    #define WIN_API_TARGET_WIN10_14393 100143930L //10.0.14393.0 //Win 10 API, build 14393
-    #define WIN_API_TARGET_WIN10_15063 100150630L //10.0.15063.0 //Win 10 API, build 15063
-	#define WIN_API_TARGET_WIN10_16299 100162990L //10.0.16299.0 //Win 10 API, build 16299
-    #define WIN_API_TARGET_WIN10_17134 100171340L //10.0.17134.0 //Win 10 API, build 17134
+    #define WIN_API_TARGET_8            80507270L //8.0.50727 //Added in case we need it trying to support older compiler or MinGW against older Windows API (VS2012 API version)
+    #define WIN_API_TARGET_81           81516360L //8.1.51636 Included with VS2013 (8.1A) //In VS2013 & VS2015, you select the Windows 8.1 API as the lowest target. VS2013 doesn't offer an API higher than this.
+    #define WIN_API_TARGET_81_AND_OLDER 81000000L //8.1.00000.0 //This is left here for compatibility before adding the definitions above.
+    #define WIN_API_TARGET_WIN10_10240  100102400L //10.0.10240.0 //Win 10 API, build 10240
+    #define WIN_API_TARGET_WIN10_10586  100105860L //10.0.10586.0 //Win 10 API, build 10586
+    #define WIN_API_TARGET_WIN10_14393  100143930L //10.0.14393.0 //Win 10 API, build 14393
+    #define WIN_API_TARGET_WIN10_15063  100150630L //10.0.15063.0 //Win 10 API, build 15063
+	#define WIN_API_TARGET_WIN10_16299  100162990L //10.0.16299.0 //Win 10 API, build 16299
+    #define WIN_API_TARGET_WIN10_17134  100171340L //10.0.17134.0 //Win 10 API, build 17134
 
 
         
