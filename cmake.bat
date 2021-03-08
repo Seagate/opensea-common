2907b2bdc94a9b55831a640648198ae5e3ca0bba
pranali-tirkhunde <pranali.tirkhunde@seagate.com>
2021-03-08 12:57:58 +0530

    quick: Updating cmake.bat

--- cmake.bat
+++ cmake.bat
@@ -1,17 +1,17 @@
 @echo off
 
-set /p VS-Version= "Enter VS Version: "
+set /p VS-Version= "Enter VS Version: (For eg: VS-2017 or VS-2015)"
 echo '%VS-Version%' 
 if not exist "%VS-Version%" (mkdir "%VS-Version%")
 
 
 if "%1" == "-x64" goto 64bit
 
-if %1 == "-x86" goto 32bit
+if "%1" == "-x86" goto 32bit
 
-if %1 == "-arm" goto arm
+if "%1" == "-arm" goto arm
 
-if %1 == "-arm64" goto arm64
+if "%1" == "-arm64" goto arm64
 
 :64bit
 if not exist "%VS-Version%/x64" (mkdir "%VS-Version%/x64")
