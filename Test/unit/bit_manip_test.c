1652fe8f3b3e556975e7462c02df89965f74a5cb
seagate <pratiksha.r.hire@seagate.com>
2026-02-06 15:22:36 +0530

    Merged changes from develop

--- Test/unit/bit_manip_test.c
+++ Test/unit/bit_manip_test.c
@@ -6,3 +6,4 @@ void run_bit_manip_tests(void)
     TEST_ASSERT_EQ(get_DWord0(0x123456789ABCDEF0ULL), 0x9ABCDEF0U, "get_DWord0: lower 32 bits");
     TEST_ASSERT_EQ(get_DWord1(0x123456789ABCDEF0ULL), 0x12345678U, "get_DWord1: upper 32 bits");
 }
+
