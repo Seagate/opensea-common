ef39a753e06768dd8ebd66ffed4a418134d91208
seagate <pratiksha.r.hire@seagate.com>
2026-02-09 14:21:50 +0530

    Updated bit_manip_test.c

--- Test/unit/bit_manip_test.c
+++ Test/unit/bit_manip_test.c
@@ -3,39 +3,80 @@
 #include "../testConstants.h"
 
 static void test_get_DWord0(void) {
-    TEST_ASSERT_EQ(get_DWord0_uint64(SERIAL_NUM), LOWER8BYTE);
+    TEST_ASSERT_EQ(get_DWord0(SERIAL_NUM), LOWER4BYTE, "get lower 32 bits of uint64_t");
 }
 
 static void test_get_DWord1(void) {
-    TEST_ASSERT_EQ(get_DWord1(SERIAL_NUM), UPPER8BYTE);
+    TEST_ASSERT_EQ(get_DWord1(SERIAL_NUM), HIGHER4BYTE, "get upper 32 bits of uint64_t");
 }
 
 static void test_M_DoubleWordInt0(void) {
-    TEST_ASSERT_EQ(M_DoubleWordInt0(SERIAL_NUM), (int32_t)(LOWER8BYTE));
+    TEST_ASSERT_EQ(M_DoubleWordInt0(SERIAL_NUM), (int32_t)(LOWER4BYTE), "get lower 32 bits of uint64_t as int32_t");
 }
 
 static void test_M_DoubleWordInt1(void) {
-    TEST_ASSERT_EQ(M_DoubleWordInt1(SERIAL_NUM), (int32_t)(UPPER8BYTE));
+    TEST_ASSERT_EQ(M_DoubleWordInt1(SERIAL_NUM), (int32_t)(HIGHER4BYTE), "get upper 32 bits of uint64_t as int32_t");
 }
 
 static void test_get_Word0_uint64(void) {
-    TEST_ASSERT_EQ(get_Word0_uint64(SERIAL_NUM), LOWER4BYTE);
+    TEST_ASSERT_EQ(get_Word0_uint64(SERIAL_NUM), LOWER2BYTE, "get lower 16 bits of uint64_t");
 }
 
 static void test_get_Word0_uint32(void) {
-    TEST_ASSERT_EQ(get_Word0_uint32(LOWER8BYTE), LOWER4BYTE);
+    TEST_ASSERT_EQ(get_Word0_uint32(LOWER4BYTE), LOWER2BYTE, "get lower 16 bits of uint32_t");
 }
 
-static void 
+static void test_M_Word0(void) {
+    TEST_ASSERT_EQ(M_Word0(SERIAL_NUM), LOWER2BYTE, "get lower 16 bits of uint64_t");
+}
+
+// 0x00000000FFFF0000
+static void test_get_Word1_uint64(void) {
+    TEST_ASSERT_EQ(get_Word1_uint64(SERIAL_NUM), (uint16_t)((LOWER4BYTE >> 16) & 0xFFFF) , "get the second lowest 16 bits of uint64_t");
+}
+
+static void test_get_Word1_uint32(void) {
+    TEST_ASSERT_EQ(get_Word1_uint32(LOWER4BYTE), (uint16_t)((LOWER4BYTE >> 16) & 0xFFFF) , "get higher 16 bits of uint32_t");
+}
+
+static void test_M_Word1(void) {
+    TEST_ASSERT_EQ(M_Word1(SERIAL_NUM), (uint16_t)((LOWER4BYTE >> 16) & 0xFFFF) , "get the second lowest 16 bits of uint64_t");
+}
+
+// 0x0000FFFF00000000
+static void test_get_Word2_uint64(void) {
+    TEST_ASSERT_EQ(get_Word2_uint64(SERIAL_NUM), (uint16_t)(HIGHER4BYTE & 0xFFFF), "get second highest 16 bits of uint64_t");
+}
+
+static void test_M_Word2(void) {
+    TEST_ASSERT_EQ(M_Word2(SERIAL_NUM), (uint16_t)(HIGHER4BYTE & 0xFFFF), "get second highest 16 bits of uint64_t");
+}
+
+// 0xFFFF000000000000
+static void test_get_Word3_uint64(void) {
+    TEST_ASSERT_EQ(get_Word3_uint64(SERIAL_NUM), HIGHER2BYTE, "get highest 16 bits of uint64_t");
+}
+
+static void test_M_Word3(void) {
+    TEST_ASSERT_EQ(M_Word3(SERIAL_NUM), HIGHER2BYTE, "get highest 16 bits of uint64_t");
+}
 
 void run_bit_manip_tests(void)
 {
-   test_get_DWord0();
-   test_get_DWord1();
-   test_M_DoubleWordInt0();
-   test_M_DoubleWordInt1(); 
-   test_get_Word0_uint64();
-   test_get_Word0_uint32();
+    test_get_DWord0();
+    test_get_DWord1();
+    test_M_DoubleWordInt0();
+    test_M_DoubleWordInt1(); 
+    test_get_Word0_uint64();
+    test_get_Word0_uint32();
+    test_get_Word1_uint64();
+    test_get_Word1_uint32();
+    test_get_Word2_uint64();
+    test_get_Word3_uint64();
+    test_M_Word0();
+    test_M_Word1();
+    test_M_Word2();
+    test_M_Word3();
 }
 
 
