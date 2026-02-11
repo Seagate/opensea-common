30a9c8e7276360d05363aa6b6ad43162ddab17ed
seagate <pratiksha.r.hire@seagate.com>
2026-02-11 11:38:08 +0530

    Modified tests regarding sgenericint_t struct for testing

--- Test/unit/bit_manip_test.c
+++ Test/unit/bit_manip_test.c
@@ -522,6 +522,8 @@ void run_bit_manip_tests(void)
     test_M_WordsTo8ByteValue();
     test_dwords_To_Uint64();
     test_M_DWordsTo8ByteValue();
+    test_ROUNDF();
+    test_is_generic_int_valid();
 }
 
 
