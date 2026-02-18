fb5f2053b17d2bd704eae4e9bb1325da63a606eb
seagate <pratiksha.r.hire@seagate.com>
2026-02-18 14:22:31 +0530

    Added 0x00 test case to test_first_leading_one_ul and ull functions

--- Test/unit/bit_manip_test.c
+++ Test/unit/bit_manip_test.c
@@ -2450,7 +2450,8 @@ static void test_first_leading_one_ul(void) {
         0x1FFFFFFFFFFFFFFFUL,
         0x3FFFFFFFFFFFFFFFUL,
         0x7FFFFFFFFFFFFFFFUL,
-        0xFFFFFFFFFFFFFFFFUL
+        0xFFFFFFFFFFFFFFFFUL,
+        0x0000000000000000UL
     };
     uint64_t expected[] = {64, 63, 62, 61, 60, 59, 58, 57, 56, 55, 54, 53, 52, 51, 50, 49, 48, 47, 46, 45, 44, 43, 42, 41, 40, 39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
 
@@ -2526,7 +2527,8 @@ static void test_first_leading_one_ull(void) {
         0x1FFFFFFFFFFFFFFFULL,
         0x3FFFFFFFFFFFFFFFULL,
         0x7FFFFFFFFFFFFFFFULL,
-        0xFFFFFFFFFFFFFFFFULL
+        0xFFFFFFFFFFFFFFFFULL,
+        0x0000000000000000ULL
     };
     uint64_t expected[] = {64, 63, 62, 61, 60, 59, 58, 57, 56, 55, 54, 53, 52, 51, 50, 49, 48, 47, 46, 45, 44, 43, 42, 41, 40, 39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
 
