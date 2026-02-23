46bc242322c29799f8ba2a51d4b23e826b3daade
seagate <pratiksha.r.hire@seagate.com>
2026-02-23 14:07:08 +0530

    Added test function test_strcasecmp

--- Test/unit/string_utils_test.c
+++ Test/unit/string_utils_test.c
@@ -2,5 +2,12 @@
 #include"../../include/string_utils.h"
 #include"../testConstants.h"
 
+static void test_strcasecmp(void) {
+    TEST_ASSERT_EQ(strcasecmp("Pratiksha", "pratiksha"), 0, "Case insensitive strings are equal");
+    TEST_ASSERT(strcasecmp("abc", "adc") < 0, "abc < adc");
+    TEST_ASSERT(strcasecmp("adc", "abc") > 0, "adc > abc");
+}
+
 void run_string_utils_tests(void) {
+    test_strcasecmp();
 }
