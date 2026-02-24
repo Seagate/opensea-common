0183f3e9dbdc3f24f43b383ca1b0d77444ff4881
seagate <pratiksha.r.hire@seagate.com>
2026-02-24 14:00:49 +0530

    Added test functions test_signed_int_round_up_generic, test_signed_int_round_down_generic, test_unsigned_int_round_up_generic and test_unsigned_int_round_down_generic

--- Test/unit/math_utils_test.c
+++ Test/unit/math_utils_test.c
@@ -117,6 +117,46 @@ static void test_unsigned_short_round_down_generic(void) {
     TEST_ASSERT_EQ(unsigned_short_round_down_generic((uint16_t)65535, 10), 65530, "Round 65535 down to nearest multiple of 10");
 }
 
+static void test_signed_int_round_up_generic(void) {
+    TEST_ASSERT_EQ(signed_int_round_up_generic(8, 2), 8, "Round 8 up to nearest multiple of 2: Roundto is power of 2");
+    TEST_ASSERT_EQ(signed_int_round_up_generic(5, 3), 6, "Round 5 up to nearest multiple of 3");
+    TEST_ASSERT_EQ(signed_int_round_up_generic(12, 1), 12, "Round 12 up to nearest multiple of 1: no-op operation");
+    TEST_ASSERT_EQ(signed_int_round_up_generic(243, 5), 245, "Round 243 up to nearest multiple of 5");
+    TEST_ASSERT_EQ(signed_int_round_up_generic(1643, 12), 1644, "Round 1643 up to nearest multiple of 12");
+    TEST_ASSERT_EQ(signed_int_round_up_generic(9496729, 12), 9496736, "Round 9496729 up to nearest multiple of 12");
+}
+
+static void test_signed_int_round_down_generic(void) {
+    TEST_ASSERT_EQ(signed_int_round_down_generic(19, 4), 16, "Round 19 down to nearest multiple of 4: Roundto is power of 2");
+    TEST_ASSERT_EQ(signed_int_round_down_generic(5, 3), 3, "Round 5 down to nearest multiple of 3");
+    TEST_ASSERT_EQ(signed_int_round_down_generic(12, 1), 12, "Round 12 down to nearest multiple of 1: no-op operation");
+    TEST_ASSERT_EQ(signed_int_round_down_generic(243, 5), 240, "Round 243 down to nearest multiple of 5");
+    TEST_ASSERT_EQ(signed_int_round_down_generic(1643, 12), 1632, "Round 1643 down to nearest multiple of 12");
+    TEST_ASSERT_EQ(signed_int_round_down_generic(32761, 10), 32760, "Round 32761 down to nearest multiple of 10");
+    TEST_ASSERT_EQ(signed_int_round_down_generic(9496729, 10), 9496710, "Round 9496729 down to nearest multiple of 10");
+}
+
+static void test_unsigned_int_round_up_generic(void) {
+    TEST_ASSERT_EQ(unsigned_int_round_up_generic((uint16_t)8, 2), 8, "Round 8 up to nearest multiple of 2: Roundto is power of 2");
+    TEST_ASSERT_EQ(unsigned_int_round_up_generic((uint16_t)5, 3), 6, "Round 5 up to nearest multiple of 3");
+    TEST_ASSERT_EQ(unsigned_int_round_up_generic((uint16_t)12, 1), 12, "Round 12 up to nearest multiple of 1: no-op operation");
+    TEST_ASSERT_EQ(unsigned_int_round_up_generic((uint16_t)243, 5), 245, "Round 243 up to nearest multiple of 5");
+    TEST_ASSERT_EQ(unsigned_int_round_up_generic((uint16_t)1643, 12), 1644, "Round 1643 up to nearest multiple of 12");
+    TEST_ASSERT_EQ(unsigned_int_round_up_generic((uint16_t)65531, 10), 4, "Round 65531 up to nearest multiple of 10");
+    TEST_ASSERT_EQ(unsigned_int_round_up_generic((uint16_t)2147483637, 10), 2147483640, "Round 2147483637 up to nearest multiple of 10");
+}
+
+static void test_unsigned_int_round_down_generic(void) {
+    TEST_ASSERT_EQ(unsigned_int_round_down_generic((uint16_t)19, 4), 16, "Round 19 down to nearest multiple of 4: Roundto is power of 2");
+    TEST_ASSERT_EQ(unsigned_int_round_down_generic((uint16_t)5, 3), 3, "Round 5 down to nearest multiple of 3");
+    TEST_ASSERT_EQ(unsigned_int_round_down_generic((uint16_t)12, 1), 12, "Round 12 down to nearest multiple of 1: no-op operation");
+    TEST_ASSERT_EQ(unsigned_int_round_down_generic((uint16_t)243, 5), 240, "Round 243 down to nearest multiple of 5");
+    TEST_ASSERT_EQ(unsigned_int_round_down_generic((uint16_t)1643, 12), 1632, "Round 1643 down to nearest multiple of 12");
+    TEST_ASSERT_EQ(unsigned_int_round_down_generic((uint16_t)32761, 10), 32760, "Round 32761 down to nearest multiple of 10");
+    TEST_ASSERT_EQ(unsigned_int_round_down_generic((uint16_t)65535, 10), 65530, "Round 65535 down to nearest multiple of 10");
+    TEST_ASSERT_EQ(unsigned_int_round_down_generic((uint16_t)4294967295, 10), 4294967290, "Round 4294967295 down to nearest multiple of 10");
+}
+
 void run_math_utils_tests(void) {
     test_M_Min();
     test_M_Max();
@@ -131,4 +171,8 @@ void run_math_utils_tests(void) {
     test_signed_short_round_down_generic();
     test_unsigned_short_round_up_generic();
     test_unsigned_short_round_down_generic();
+    test_signed_int_round_up_generic();
+    test_signed_int_round_down_generic();
+    test_unsigned_int_round_up_generic();
+    test_unsigned_int_round_down_generic();
 }
