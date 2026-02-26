13c7ebd5e206d13850a8ea71be21c4d6c1eea4d6
seagate <pratiksha.r.hire@seagate.com>
2026-02-26 15:19:59 +0530

    Test common_String_Concat when dest pointer is null

--- Test/unit/string_utils_test.c
+++ Test/unit/string_utils_test.c
@@ -628,26 +628,21 @@ static void test_common_String_Concat(void) {
     TEST_ASSERT_EQ(common_String_Concat(dest, sizeof(dest), src), dest, "String is correctly concatenated to destination buffer");
 
     // Test for null pointer protection
-    // errno = 0;
-    // common_String_Concat(NULL, sizeof(dest), src);
-    // TEST_ASSERT_EQ(errno, ERANGE, "common_String_Concat sets errno to ERANGE when destination pointer is null");
+    errno = 0;
+    TEST_ASSERT_EQ(common_String_Concat(NULL, sizeof(dest), src), NULL, "common_String_Concat returns NULL when destination pointer is null");
     // errno = 0;   
-    // common_String_Concat(dest, sizeof(dest), NULL);
-    // TEST_ASSERT_EQ(errno, ERANGE, "common_String_Concat sets errno to ERANGE when source pointer is null");
+    // TEST_ASSERT_EQ(common_String_Concat(dest, sizeof(dest), NULL), NULL, "common_String_Concat returns NULL when source pointer is null");
 
     // Test for zero and too large destsz
+    // errno = 0;;
+    // TEST_ASSERT_EQ(common_String_Concat(dest, 0, src), NULL, "common_String_Concat returns NULL when destsz is zero");
     // errno = 0;
-    // common_String_Concat(dest, 0, src);
-    // TEST_ASSERT_EQ(errno, ERANGE, "common_String_Concat sets errno to ERANGE when destsz is zero");
-    // errno = 0;
-    // common_String_Concat(dest, RSIZE_MAX + 1, src);
-    // TEST_ASSERT_EQ(errno, ERANGE, "common_String_Concat sets errno to ERANGE when destsz is greater than RSIZE_MAX");
+    // TEST_ASSERT_EQ(common_String_Concat(dest, RSIZE_MAX + 1, src), NULL, "common_String_Concat returns NULL when destsz is greater than RSIZE_MAX");
     
     // Test for no null terminator in the first destsz bytes of dest
     // char str[20] = "This string is too long for the buffer";
     // errno = 0;   
-    // common_String_Concat(str, sizeof(str), src);
-    // TEST_ASSERT_EQ(errno, ERANGE, "common_String_Concat sets errno to ERANGE when there is no null terminator in the first destsz bytes of dest");
+    // TEST_ASSERT_EQ(common_String_Concat(str, sizeof(str), src), NULL, "common_String_Concat returns NULL when there is no null terminator in the first destsz bytes of dest");
 }
 
 void run_string_utils_tests(void) {
