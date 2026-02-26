f3715ddb093bbd9bbd6222a7bf19db59e8c83d53
seagate <pratiksha.r.hire@seagate.com>
2026-02-26 10:10:43 +0530

    Test safe_strmove when dest pointer is null: test should fail

--- Test/unit/string_utils_test.c
+++ Test/unit/string_utils_test.c
@@ -451,10 +451,20 @@ static void test_safe_strmove(void) {
     TEST_ASSERT_EQ(strcmp(str, "ThisString"), 0, "String should be shifted left correctly");
 
     // Test for buffer overflow protection
-    char smallDest[5];
+    // char smallDest[5];
+    // errno = 0;
+    // safe_strcpy(smallDest, sizeof(smallDest), src);
+    // TEST_ASSERT_EQ(errno, ERANGE, "safe_strcpy sets errno to ERANGE when destination buffer is too small");
+
+    // Test for null pointer protection
     errno = 0;
-    safe_strcpy(smallDest, sizeof(smallDest), src);
-    TEST_ASSERT_EQ(errno, ERANGE, "safe_strcpy sets errno to ERANGE when destination buffer is too small");
+    safe_strcpy(NULL, sizeof(dest), src);
+    TEST_ASSERT_EQ(errno, ERANGE, "safe_strcpy sets errno to ERANGE when destination pointer is null");
+    errno = 0;
+    src = NULL;
+    safe_strcpy(dest, sizeof(dest), NULL);
+    TEST_ASSERT_EQ(errno, ERANGE, "safe_strcpy sets errno to ERANGE when source pointer is null");
+
 }
 
 void run_string_utils_tests(void) {
