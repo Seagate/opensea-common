b9c6bce633043597bcdce7b453e17ae2a466a53a
seagate <pratiksha.r.hire@seagate.com>
2026-02-27 11:28:09 +0530

    Testing safe_strdup when src is NULL: test should fail

--- Test/unit/string_utils_test.c
+++ Test/unit/string_utils_test.c
@@ -752,10 +752,11 @@ static void test_strndup(void) {
 }
 
 static void test_safe_strdup(void) {
+    // Test when src is NULL
     const char* str = "Hello, World!";
     char* dup;
 
-    errno_t err = safe_strdup(&dup, str);
+    errno_t err = safe_strdup(&dup, NULL);
 
     TEST_ASSERT_EQ(err, 0, "Duplication should succeed");
     TEST_ASSERT_EQ(strcmp(dup, str), 0, "String should be correctly duplicated");
