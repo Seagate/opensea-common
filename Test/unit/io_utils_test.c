4c1959acce771c259f6f00f6b75cdc862b9e0776
seagate <pratiksha.r.hire@seagate.com>
2026-03-10 11:19:35 +0530

    Testing function get_And_Validate_Integer_Input

--- Test/unit/io_utils_test.c
+++ Test/unit/io_utils_test.c
@@ -6,7 +6,7 @@ static void test_get_And_Validate_Integer_Input(void) {
     uint64_t outputInteger;
     TEST_ASSERT(get_And_Validate_Integer_Input("12345", &outputInteger), "Converted string to integer successfully");
     TEST_ASSERT(get_And_Validate_Integer_Input("0xFF", &outputInteger), "Converted string to integer successfully");
-    TEST_ASSERT(!get_And_Validate_Integer_Input("123abcd", &outputInteger), "Could not convert string to integer successfully");
+    TEST_ASSERT(!get_And_Validate_Integer_Input("xyz", &outputInteger), "Could not convert string to integer successfully");
     TEST_ASSERT(!get_And_Validate_Integer_Input("12.5", &outputInteger), "Could not convert string to integer successfully");
 }
 
