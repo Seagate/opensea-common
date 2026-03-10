9bfc36e373ae416a77ee14ef7294b0a27a639d14
seagate <pratiksha.r.hire@seagate.com>
2026-03-10 11:29:59 +0530

    Added test function test_get_And_Validate_Integer_Input_Uint64

--- Test/unit/io_utils_test.c
+++ Test/unit/io_utils_test.c
@@ -10,6 +10,16 @@ static void test_get_And_Validate_Integer_Input(void) {
     TEST_ASSERT(!get_And_Validate_Integer_Input("12.5", &outputInteger), "Could not convert string to integer successfully");
 }
 
+static void test_get_And_Validate_Integer_Input_Uint64(void) {
+    uint64_t outputInteger;
+    TEST_ASSERT(get_And_Validate_Integer_Input_Uint64("0xFF", NULL, ALLOW_UNIT_NONE, &outputInteger), "Converted string to integer successfully");
+    TEST_ASSERT(get_And_Validate_Integer_Input_Uint64("12ms", NULL, ALLOW_UNIT_SECTOR_TYPE, &outputInteger), "Converted string to integer successfully");
+    TEST_ASSERT(!get_And_Validate_Integer_Input_Uint64("xyz", NULL, ALLOW_UNIT_NONE, &outputInteger), "Could not convert string to integer successfully");
+    TEST_ASSERT(!get_And_Validate_Integer_Input_Uint64("123KB", NULL, ALLOW_UNIT_NONE, &outputInteger), "Could not convert string to integer successfully");
+    TEST_ASSERT(!get_And_Validate_Integer_Input_Uint64("12.5", NULL, ALLOW_UNIT_NONE, &outputInteger), "Could not convert string to integer successfully");
+}
+
 void run_io_utils_tests(void) {
     test_get_And_Validate_Integer_Input();
+    test_get_And_Validate_Integer_Input_Uint64();
 }
