0d593d613d485c40f5661ce39adb50d7af5000ae
seagate <pratiksha.r.hire@seagate.com>
2026-03-10 13:21:49 +0530

    Made changes to test_get_And_Validate_Integer_Input_Uint8

--- Test/unit/io_utils_test.c
+++ Test/unit/io_utils_test.c
@@ -46,7 +46,7 @@ static void test_get_And_Validate_Integer_Input_Uint8(void) {
     uint8_t outputInteger;
     char* unit = NULL;
     TEST_ASSERT(get_And_Validate_Integer_Input_Uint8("0xFF", NULL, ALLOW_UNIT_NONE, &outputInteger), "Converted string to integer successfully");
-    TEST_ASSERT(get_And_Validate_Integer_Input_Uint8("123f", &unit, ALLOW_UNIT_TEMPERATURE, &outputInteger), "Converted string to integer successfully");
+    TEST_ASSERT(get_And_Validate_Integer_Input_Uint8("20f", &unit, ALLOW_UNIT_TEMPERATURE, &outputInteger), "Converted string to integer successfully");
     TEST_ASSERT(get_And_Validate_Integer_Input_Uint8("12ms", &unit, ALLOW_UNIT_TIME, &outputInteger), "Converted string to integer successfully");
     TEST_ASSERT(!get_And_Validate_Integer_Input_Uint8("xyz", NULL, ALLOW_UNIT_NONE, &outputInteger), "Could not convert string to integer successfully");
     TEST_ASSERT(!get_And_Validate_Integer_Input_Uint8("123KB", NULL, ALLOW_UNIT_NONE, &outputInteger), "Could not convert string to integer successfully");
