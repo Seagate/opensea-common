ccf6510702f4d6808e78f1a214729d5a9f66330a
seagate <pratiksha.r.hire@seagate.com>
2026-03-12 15:02:46 +0530

    Debugging test_safe_strmove

--- Test/unit/string_utils_test.c
+++ Test/unit/string_utils_test.c
@@ -448,6 +448,7 @@ static void test_safe_strmove(void) {
     // Move "String" one position left (overwrite space)
     errno_t err = safe_strmove(str + 4, sizeof(str) - 4, str + 5);
     TEST_ASSERT_EQ(err, 0, "Move should succeed");
+    printf("str after move: %s\n", str);
     TEST_ASSERT_EQ(strcmp(str, "ThisStringg"), 0, "String should be shifted left correctly");
 
     // Test for buffer overflow protection
