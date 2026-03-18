9cefb096d46be8f17bc1229e96f5c1cc26b08caa
seagate <pratiksha.r.hire@seagate.com>
2026-03-18 14:31:20 +0530

    Added test function test_safe_free_schar

--- Test/unit/memory_safety_test.c
+++ Test/unit/memory_safety_test.c
@@ -87,6 +87,12 @@ static void test_safe_free_char(void) {
     TEST_ASSERT(ptr == NULL, "safe_free_char should set the pointer to NULL after freeing");
 }
 
+static void test_safe_free_schar(void) {
+    signed char* ptr = safe_malloc(100);
+    safe_free_schar(&ptr);
+    TEST_ASSERT(ptr == NULL, "safe_free_schar should set the pointer to NULL after freeing");
+}
+
 void run_memory_safety_tests(void) {
     test_safe_malloc();
     test_safe_calloc();
@@ -94,4 +100,5 @@ void run_memory_safety_tests(void) {
     test_safe_reallocf();
     test_safe_free_core();
     test_safe_free_char();
+    test_safe_free_schar();
 }
