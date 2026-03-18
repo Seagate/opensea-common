7a40133e5bfc64f36e767af2894e29f0e12aecf1
seagate <pratiksha.r.hire@seagate.com>
2026-03-18 13:49:33 +0530

    Added test function test_safe_calloc

--- Test/unit/memory_safety_test.c
+++ Test/unit/memory_safety_test.c
@@ -13,6 +13,23 @@ static void test_safe_malloc(void) {
     // free(ptr);
 }
 
+static void test_safe_calloc(void) {
+    void* ptr = safe_calloc(10, 20);
+    TEST_ASSERT(ptr != NULL, "safe_calloc should return a non-null pointer for non-zero count and size");
+    free(ptr);
+
+    // Test that safe_calloc returns null for a count of zero
+    // ptr = safe_calloc(0, 20);
+    // TEST_ASSERT(ptr == NULL, "safe_calloc should return a null pointer for a count of zero");
+    // free(ptr);
+
+    // Test that safe_calloc returns null for a size of zero
+    // ptr = safe_calloc(10, 0);
+    // TEST_ASSERT(ptr == NULL, "safe_calloc should return a null pointer for a size of zero");
+    // free(ptr);
+}
+
 void run_memory_safety_tests(void) {
     test_safe_malloc();
+    test_safe_calloc();
 }
