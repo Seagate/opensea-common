83f74c76f4e5f241c1f117ae3d164e943585cfa9
seagate <pratiksha.r.hire@seagate.com>
2026-03-18 14:05:56 +0530

    Added test function test_safe_reallocf

--- Test/unit/memory_safety_test.c
+++ Test/unit/memory_safety_test.c
@@ -44,8 +44,24 @@ static void test_safe_realloc(void) {
     TEST_ASSERT(new_ptr == NULL, "safe_realloc should return a null pointer when reallocating to zero");
 }
 
+static void test_safe_reallocf(void) {
+    void* ptr = safe_malloc(100);
+    TEST_ASSERT(ptr != NULL, "safe_malloc should return a non-null pointer for a non-zero size");
+
+    // Test that reallocating to a larger size works
+    void* new_ptr = safe_reallocf(&ptr, 200);
+    TEST_ASSERT(new_ptr != NULL, "safe_reallocf should return a non-null pointer when reallocating to a larger size");
+    free(new_ptr);
+
+    // Test that reallocating to zero frees the memory and returns null
+    ptr = safe_malloc(100);
+    new_ptr = safe_reallocf(&ptr, 0);
+    TEST_ASSERT(new_ptr == NULL, "safe_reallocf should return a null pointer when reallocating to zero");
+}
+
 void run_memory_safety_tests(void) {
     test_safe_malloc();
     test_safe_calloc();
     test_safe_realloc();
+    test_safe_reallocf();
 }
