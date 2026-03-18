737b0b4e78e3d0544dd03af3efbb9fb775edff77
seagate <pratiksha.r.hire@seagate.com>
2026-03-18 13:36:09 +0530

    Added file memory_safety_test.c

---
+++ Test/unit/memory_safety_test.c
@@ -0,0 +1,7 @@
+#include"../framework/test_framework.h"
+#include"../../include/io_utils.h"
+#include"../testConstants.h"
+
+void run_io_utils_tests(void) {
+    printf("Running io_utils tests...\n");
+}
