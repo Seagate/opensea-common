719ce257839cb180b0ddcbf3ac77716c69430881
seagate <pratiksha.r.hire@seagate.com>
2026-03-18 10:31:23 +0530

    Testing test_print_Data_Buffer for non-printable chars

--- Test/unit/io_utils_test.c
+++ Test/unit/io_utils_test.c
@@ -520,7 +520,7 @@ static void test_set_Console_Colors(void) {
 }
 
 static void test_print_Data_Buffer(void) {
-    uint8_t data[] = {0xDE, 0xAD, 0xBE, 0xEF, "\n"};
+    uint8_t data[] = {0xDE, 0xAD, 0xBE, 0xEF, 0x09};
     print_Data_Buffer(data, sizeof(data), true);
 }
 
