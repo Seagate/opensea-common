adbf5421f81e9ab451942cefef3bbc1325e7cab1
seagate <pratiksha.r.hire@seagate.com>
2026-03-18 13:43:23 +0530

    Added test function test_safe_malloc

--- Test/unit/memory_safety_test.c
+++ Test/unit/memory_safety_test.c
@@ -2,6 +2,16 @@
 #include"../../include/io_utils.h"
 #include"../testConstants.h"
 
+static void test_safe_malloc(void) {
+    void* ptr = safe_malloc(100);
+    TEST_ASSERT(ptr != NULL, "safe_malloc should return a non-null pointer for a non-zero size");
+    free(ptr);
+
+    // Test that safe_malloc returns null for a size of zero
+    ptr = safe_malloc(0);
+    TEST_ASSERT(ptr == NULL, "safe_malloc should return a null pointer for a size of zero");
+}
+
 void run_memory_safety_tests(void) {
-    printf("Running memory_safety tests...\n");
+    test_safe_malloc();
 }
