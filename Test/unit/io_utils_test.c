509b763214148fad074192235d68bfa72c3b9f8a
seagate <pratiksha.r.hire@seagate.com>
2026-03-24 10:18:12 +0530

    Testing function test_safe_strtol when string is NULL: test should fail

--- Test/unit/io_utils_test.c
+++ Test/unit/io_utils_test.c
@@ -792,7 +792,7 @@ static void test_safe_gets(void)
 static void test_safe_strtol(void) {
     long result;
     char *endptr;
-    errno_t err = safe_strtol(NULL, "12345", &endptr, 10);
+    errno_t err = safe_strtol(&result, NULL, &endptr, 10);
     TEST_ASSERT(result == 12345, "safe_strtol converted string to long correctly");
     TEST_ASSERT(*endptr == '\0', "safe_strtol consumed the entire string");
     TEST_ASSERT(errno == 0, "safe_strtol did not set errno for valid input");
