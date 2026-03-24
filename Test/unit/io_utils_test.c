fc8fd1c2df7c9b04fa36ecf6051550b7a0b80d69
seagate <pratiksha.r.hire@seagate.com>
2026-03-24 10:26:09 +0530

    Testing function test_safe_strtol with base 2

--- Test/unit/io_utils_test.c
+++ Test/unit/io_utils_test.c
@@ -792,7 +792,13 @@ static void test_safe_gets(void)
 static void test_safe_strtol(void) {
     long result;
     char *endptr;
-    errno_t err = safe_strtol(&result, "12345", &endptr, 3);
+    errno_t err = safe_strtol(&result, "12345", &endptr, 10);
+    TEST_ASSERT(result == 12345, "safe_strtol converted string to long correctly");
+    TEST_ASSERT(*endptr == '\0', "safe_strtol consumed the entire string");
+    TEST_ASSERT(errno == 0, "safe_strtol did not set errno for valid input");
+
+    errno_t err = safe_strtol(&result, "12345", &endptr, 2);
+    printf("Result: %ld, Endptr: '%s', Errno: %d\n", result, endptr, errno);
     TEST_ASSERT(result == 12345, "safe_strtol converted string to long correctly");
     TEST_ASSERT(*endptr == '\0', "safe_strtol consumed the entire string");
     TEST_ASSERT(errno == 0, "safe_strtol did not set errno for valid input");
