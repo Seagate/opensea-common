696d2c13229ddf49e9bf87a312616e14e5f44d16
seagate <pratiksha.r.hire@seagate.com>
2026-03-25 14:19:45 +0530

    Debugging function test_malloc_page_aligned for size = 0

--- Test/unit/memory_safety_test.c
+++ Test/unit/memory_safety_test.c
@@ -527,6 +527,7 @@ static void test_malloc_page_aligned(void) {
 
     // Allocating size of zero should return null
     ptr = malloc_page_aligned(0);   
+    printf("ptr: %p\n", (void*)ptr);
     TEST_ASSERT(ptr == NULL, "malloc_page_aligned should return a null pointer for a size of zero");
     free_page_aligned(ptr);
 }
