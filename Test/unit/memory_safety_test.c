689267d2d36079fa76b65c17284beb2f3be5ea89
seagate <pratiksha.r.hire@seagate.com>
2026-03-25 11:09:05 +0530

    Added test function test_safe_free_aligned_core

--- Test/unit/memory_safety_test.c
+++ Test/unit/memory_safety_test.c
@@ -279,6 +279,13 @@ static void test_malloc_aligned(void) {
     free_aligned(ptr);
 }
 
+static void test_safe_free_aligned_core(void) {
+    char* ptr = malloc_aligned(100, 16);
+    TEST_ASSERT(ptr != NULL, "malloc_aligned should return a non-null pointer for a non-zero size");
+    safe_free_aligned_core((void**)&ptr);
+    TEST_ASSERT(ptr == NULL, "safe_free_aligned_core should set the pointer to NULL after freeing");
+}
+
 void run_memory_safety_tests(void) {
     test_safe_malloc();
     test_safe_calloc();
@@ -309,4 +316,5 @@ void run_memory_safety_tests(void) {
     test_explicit_zeroes();
     test_free_aligned();
     test_malloc_aligned();
+    test_safe_free_aligned_core();
 }
