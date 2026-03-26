f773fa65f4e1b444d1cf9f4c723be74ce13989a6
seagate <pratiksha.r.hire@seagate.com>
2026-03-26 09:46:15 +0530

    Testing function test_safe_memcpy_no_overlap when dest is NULL ptr: test should fail

--- Test/unit/memory_safety_test.c
+++ Test/unit/memory_safety_test.c
@@ -672,7 +672,7 @@ static void test_safe_memcpy(void) {
 static void test_safe_memcpy_no_overlap(void) {
     char src[20] = "Hello, World!";
     char dest[10];
-    errno_t result = safe_memcpy_no_overlap(dest, sizeof(dest), src, 10);
+    errno_t result = safe_memcpy_no_overlap(NULL, sizeof(dest), src, 10);
     TEST_ASSERT(result == 0, "safe_memcpy_no_overlap should return zero on success");
     TEST_ASSERT(strncmp(dest, src, 10) == 0, "safe_memcpy_no_overlap should copy the correct data");
 }
