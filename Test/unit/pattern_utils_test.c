e81a5e8e6bbf05dbade4d609c38c411ffda65949
seagate <pratiksha.r.hire@seagate.com>
2026-03-26 12:18:26 +0530

    Testing function test_fill_Random_Pattern_In_Buffer

--- Test/unit/pattern_utils_test.c
+++ Test/unit/pattern_utils_test.c
@@ -5,7 +5,7 @@
 static void test_fill_Random_Pattern_In_Buffer(void) {
     // Test with valid parameters
     uint8_t buffer[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    eReturnValues result = fill_Random_Pattern_In_Buffer(buffer, RSIZE_MAX);
+    eReturnValues result = fill_Random_Pattern_In_Buffer(buffer, sizeof(buffer));
     TEST_ASSERT(result == SUCCESS, "Expected SUCCESS for valid parameters");
     printf("Buffer after fill_Random_Pattern_In_Buffer: ");
     for (size_t i = 0; i < sizeof(buffer); i++) {
