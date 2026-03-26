35961aa29d35711c8ca63c2da7f7b99484ffe16a
seagate <pratiksha.r.hire@seagate.com>
2026-03-26 14:46:22 +0530

    Added test function test_fill_Pattern_Buffer_Into_Another_Buffer

--- Test/unit/pattern_utils_test.c
+++ Test/unit/pattern_utils_test.c
@@ -85,9 +85,22 @@ static void test_fill_ASCII_Pattern_In_Buffer(void) {
     TEST_ASSERT(result == BAD_PARAMETER, "Returns BAD_PARAMETER for null pointer");
 }
 
+static void test_fill_Pattern_Buffer_Into_Another_Buffer(void) {
+    uint8_t inPattern[5] = {0x01, 0x02, 0x03, 0x04, 0x05};
+    uint8_t outBuffer[10];
+    eReturnValues result = fill_Pattern_Buffer_Into_Another_Buffer(inPattern, sizeof(inPattern), outBuffer, sizeof(outBuffer));
+    TEST_ASSERT(result == SUCCESS, "Expected SUCCESS for valid parameters");
+    printf("Buffer after fill_Pattern_Buffer_Into_Another_Buffer: ");
+    for (size_t i = 0; i < sizeof(outBuffer); i++) {
+        printf("%02X ", outBuffer[i]);
+    }
+    printf("\n");
+}
+
 void run_pattern_utils_tests(void) {
     test_fill_Random_Pattern_In_Buffer();
     test_fill_Hex_Pattern_In_Buffer();
     test_fill_Incrementing_Pattern_In_Buffer();
     test_fill_ASCII_Pattern_In_Buffer();
+    test_fill_Pattern_Buffer_Into_Another_Buffer();
 }
