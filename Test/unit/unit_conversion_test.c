8ab5f29496180643aca549cef9a22672ff6a4adb
seagate <pratiksha.r.hire@seagate.com>
2026-03-30 11:03:31 +0530

    Added test function test_metric_Unit_Convert

--- Test/unit/unit_conversion_test.c
+++ Test/unit/unit_conversion_test.c
@@ -2,6 +2,15 @@
 #include"../../include/unit_conversion.h"
 #include"../testConstants.h"
 
+static void test_metric_Unit_Convert() {
+    double byteValue = 8000 * 1000 * 1000 * 1000; // 8000 GB in bytes
+    char metricUnit[UNIT_STRING_LENGTH];
+    eReturnValues result = metric_Unit_Convert(&byteValue, &metricUnit);
+    TEST_ASSERT(result == SUCCESS, "Expected SUCCESS, converted 8000 GB to 8TB");
+    TEST_ASSERT(strcmp(metricUnit, "TB") == 0, "Expected metricUnit is TB");
+    TEST_ASSERT(byteValue == 8, "Expected byteValue is 8");
+}
+
 void run_unit_conversion_tests() {
-    printf("Running unit conversion tests...\n");
+    test_metric_Unit_Convert();
 }
