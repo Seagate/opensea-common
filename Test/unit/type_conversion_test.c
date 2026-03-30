b8f1a7054ff575fb0e3433f033901d062f48de92
seagate <pratiksha.r.hire@seagate.com>
2026-03-30 10:26:11 +0530

    Debugging function test_convert_128bit_to_double

--- Test/unit/type_conversion_test.c
+++ Test/unit/type_conversion_test.c
@@ -653,12 +653,12 @@ static void test_convert_128bit_to_double(void) {
     double result = convert_128bit_to_double(array);
     TEST_ASSERT(result == 0.0, "convert_128bit_to_double should convert an array of zeros to 0.0");
 
-    array[15] = 1;
+    array[0] = 1;
     result = convert_128bit_to_double(array);
     printf("result for 1: %f\n", result);
     TEST_ASSERT(result == 1.0, "128-bit value 1 should convert correctly");
 
-    array[15] = 255;
+    array[0] = 255;
     result = convert_128bit_to_double(array);
     printf("result for 255: %f\n", result);
     TEST_ASSERT(result == 255.0, "128-bit value 255 should convert correctly");
