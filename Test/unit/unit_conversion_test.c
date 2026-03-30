3969bbdb2bad615c5cefdbc2455f629e11db919e
seagate <pratiksha.r.hire@seagate.com>
2026-03-30 11:29:57 +0530

    Debugging function test_metric_Unit_Convert

--- Test/unit/unit_conversion_test.c
+++ Test/unit/unit_conversion_test.c
@@ -8,6 +8,7 @@ static void test_metric_Unit_Convert() {
     char* metricUnit = metricUnitBuffer;
     eReturnValues result = metric_Unit_Convert(&byteValue, &metricUnit);
     TEST_ASSERT(result == SUCCESS, "Expected SUCCESS, converted 8000 GB to 8TB");
+    printf("Converted byteValue: %f, metricUnit: %s\n", byteValue, metricUnit);
     TEST_ASSERT(strcmp(metricUnit, "TB") == 0, "Expected metricUnit is TB");
     TEST_ASSERT(byteValue == 8, "Expected byteValue is 8");
 }
