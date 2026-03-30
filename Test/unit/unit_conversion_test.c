9719639b3b210f0d8f9db87d9a5d408081467b53
seagate <pratiksha.r.hire@seagate.com>
2026-03-30 11:41:33 +0530

    Debugging function test_metric_Unit_Convert

--- Test/unit/unit_conversion_test.c
+++ Test/unit/unit_conversion_test.c
@@ -3,13 +3,49 @@
 #include"../testConstants.h"
 
 static void test_metric_Unit_Convert() {
-    double byteValue = 8000.0 * 1000 * 1000 * 1000; // 8000 GB in bytes
+    double byteValue = (double)8;
     char metricUnitBuffer[UNIT_STRING_LENGTH];
     char* metricUnit = metricUnitBuffer;
+
     eReturnValues result = metric_Unit_Convert(&byteValue, &metricUnit);
-    TEST_ASSERT(result == SUCCESS, "Expected SUCCESS, converted 8000 GB to 8TB");
-    printf("Converted byteValue: %f, metricUnit: %s\n", byteValue, metricUnit);
+    TEST_ASSERT(result == SUCCESS, "Expected SUCCESS, converted 8 B to 8B");
+    TEST_ASSERT(strcmp(metricUnit, "B") == 0, "Expected metricUnit is B");
+    TEST_ASSERT(byteValue == 8, "Expected byteValue is 8");
+
+    byteValue = (double)8000;
+    result = metric_Unit_Convert(&byteValue, &metricUnit);
+    TEST_ASSERT(result == SUCCESS, "Expected SUCCESS, converted 8000 B to 8KB");
+    TEST_ASSERT(strcmp(metricUnit, "KB") == 0, "Expected metricUnit is KB");
+    TEST_ASSERT(byteValue == 8, "Expected byteValue is 8");
+
+    byteValue = (double)8000000;
+    result = metric_Unit_Convert(&byteValue, &metricUnit);
+    TEST_ASSERT(result == SUCCESS, "Expected SUCCESS, converted 8000000 B to 8MB");
+    TEST_ASSERT(strcmp(metricUnit, "MB") == 0, "Expected metricUnit is MB");
+    TEST_ASSERT(byteValue == 8, "Expected byteValue is 8");
+
+    byteValue = (double)8000000000;
+    result = metric_Unit_Convert(&byteValue, &metricUnit);
+    TEST_ASSERT(result == SUCCESS, "Expected SUCCESS, converted 8000000000 B to 8GB");
+    TEST_ASSERT(strcmp(metricUnit, "GB") == 0, "Expected metricUnit is GB");
+    TEST_ASSERT(byteValue == 8, "Expected byteValue is 8");
+
+    byteValue = (double)8000000000000;
+    result = metric_Unit_Convert(&byteValue, &metricUnit);
+    TEST_ASSERT(result == SUCCESS, "Expected SUCCESS, converted 8000000000000 B to 8TB");
     TEST_ASSERT(strcmp(metricUnit, "TB") == 0, "Expected metricUnit is TB");
+    TEST_ASSERT(byteValue == 8, "Expected byteValue is 8");
+
+    byteValue = (double)8000000000000000;
+    result = metric_Unit_Convert(&byteValue, &metricUnit);
+    TEST_ASSERT(result == SUCCESS, "Expected SUCCESS, converted 8000000000000000 B to 8PB");
+    TEST_ASSERT(strcmp(metricUnit, "PB") == 0, "Expected metricUnit is PB");
+    TEST_ASSERT(byteValue == 8, "Expected byteValue is 8");
+
+    byteValue = (double)8000000000000000000;
+    result = metric_Unit_Convert(&byteValue, &metricUnit);
+    TEST_ASSERT(result == SUCCESS, "Expected SUCCESS, converted 8000000000000000000 B to 8EB");
+    TEST_ASSERT(strcmp(metricUnit, "EB") == 0, "Expected metricUnit is EB");
     TEST_ASSERT(byteValue == 8, "Expected byteValue is 8");
 }
 
