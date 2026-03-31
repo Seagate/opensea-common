c931cdb75de77f9444f8c73a5bc94f0bf8bb6b32
seagate <pratiksha.r.hire@seagate.com>
2026-03-31 11:52:26 +0530

    Checking code coverage for pattern_utils.h

--- Test/main.c
+++ Test/main.c
@@ -6,8 +6,8 @@
 // void run_math_utils_tests(void);
 // void run_string_utils_tests(void);
 // void run_io_utils_tests(void);
-void run_memory_safety_tests(void);
-// void run_pattern_utils_tests(void);
+// void run_memory_safety_tests(void);
+void run_pattern_utils_tests(void);
 // void run_sort_and_search_tests(void);
 // void run_type_conversion_tests(void);
 // void run_unit_conversion_tests(void);
@@ -21,8 +21,8 @@ int main(void)
     // run_math_utils_tests();
     // run_string_utils_tests();
     // run_io_utils_tests();
-    run_memory_safety_tests();
-    // run_pattern_utils_tests();
+    // run_memory_safety_tests();
+    run_pattern_utils_tests();
     // run_sort_and_search_tests();
     // run_type_conversion_tests();
     // run_unit_conversion_tests();
