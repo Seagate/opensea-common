846259a7447bddb3235a33fdf4bdbe5f2da3eb62
seagate <pratiksha.r.hire@seagate.com>
2026-03-31 13:47:18 +0530

    Changes to meson.yml

--- Test/main.c
+++ Test/main.c
@@ -2,7 +2,7 @@
 #include "framework/test_framework.h"
 
 // Forward declarations of test functions
-// void run_bit_manip_tests(void);
+void run_bit_manip_tests(void);
 // void run_math_utils_tests(void);
 // void run_string_utils_tests(void);
 // void run_io_utils_tests(void);
@@ -10,14 +10,14 @@
 // void run_pattern_utils_tests(void);
 // void run_sort_and_search_tests(void);
 // void run_type_conversion_tests(void);
-void run_unit_conversion_tests(void);
+// void run_unit_conversion_tests(void);
 
 int main(void)
 {
     printf("Starting unit tests...\n\n");
 
     // Run individual test suites
-    // run_bit_manip_tests();
+    run_bit_manip_tests();
     // run_math_utils_tests();
     // run_string_utils_tests();
     // run_io_utils_tests();
@@ -25,7 +25,7 @@ int main(void)
     // run_pattern_utils_tests();
     // run_sort_and_search_tests();
     // run_type_conversion_tests();
-    run_unit_conversion_tests();
+    // run_unit_conversion_tests();
     
     // Print summary of test results
     print_test_summary();
