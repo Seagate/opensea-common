cbec8dd3191e0e88f6334cf939f155e040f0d9f3
seagate <pratiksha.r.hire@seagate.com>
2026-04-02 11:51:43 +0530

    Made changes to functions test_get_8bit_range_uint64, test_get_16bit_range_uint64 and test_get_32bit_range_uint64

--- Test/unit/bit_manip_test.c
+++ Test/unit/bit_manip_test.c
@@ -539,7 +539,7 @@ static void test_get_bit_range_uint64(void) {
     TEST_ASSERT(errno == ERANGE, "get_bit_range_uint64 should set errno to ERANGE when msb > 63");
 
     // Test for lsb > GENERIC_INT_64BIT_MAX
-    get_bit_range_uint64(HEX_RANDOM, 63, 64);
+    get_bit_range_uint64(HEX_RANDOM, 60, 64);
     TEST_ASSERT(errno == ERANGE, "get_bit_range_uint64 should set errno to ERANGE when lsb > 63");
 
     // Test for bitcount = 0
@@ -552,14 +552,29 @@ static void test_get_bit_range_uint64(void) {
 
 static void test_get_8bit_range_uint64(void) {
     TEST_ASSERT_EQ(get_8bit_range_uint64(HEX_RANDOM, 31, 24), (uint8_t)(0xF0), "Extract bits 31 to 24 from 0xF0F0F0F0F0F0F0F0ULL");
+
+    // Test for bit_count > GENERIC_WIDTH_8
+    errno = 0;
+    get_8bit_range_uint64(HEX_RANDOM, 15, 0);
+    TEST_ASSERT(errno == ERANGE, "get_8bit_range_uint64 should set errno to ERANGE when bit_count > 8");
 }
 
 static void test_get_16bit_range_uint64(void) {
     TEST_ASSERT_EQ(get_16bit_range_uint64(HEX_RANDOM, 39, 24), (uint16_t)(0xF0F0), "Extract bits 39 to 24 from 0xF0F0F0F0F0F0F0F0ULL");
+
+    // Test for bit_count > GENERIC_WIDTH_16
+    errno = 0;
+    get_16bit_range_uint64(HEX_RANDOM, 31, 0);
+    TEST_ASSERT(errno == ERANGE, "get_16bit_range_uint64 should set errno to ERANGE when bit_count > 16");
 }
 
 static void test_get_32bit_range_uint64(void) {
     TEST_ASSERT_EQ(get_32bit_range_uint64(HEX_RANDOM, 63, 32), (uint32_t)(0xF0F0F0F0), "Extract bits 63 to 32 from 0xF0F0F0F0F0F0F0F0ULL");
+
+    // Test for bit_count > GENERIC_WIDTH_32
+    errno = 0;
+    get_32bit_range_uint64(HEX_RANDOM, 63, 0);
+    TEST_ASSERT(errno == ERANGE, "get_32bit_range_uint64 should set errno to ERANGE when bit_count > 32");
 }
 
 static void test_get_bit_range_int8(void) {
