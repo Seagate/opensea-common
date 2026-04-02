2a20fe3104cc7c89a2be0e15763fbb9204c572e8
seagate <pratiksha.r.hire@seagate.com>
2026-04-02 14:55:45 +0530

    Made changes to functions test_signed_short_round_up_generic, test_signed_short_round_down_generic, test_signed_int_round_up_generic and test_signed_int_round_down_generic

--- Test/unit/math_utils_test.c
+++ Test/unit/math_utils_test.c
@@ -77,6 +77,7 @@ static void test_unsigned_char_round_down_generic(void) {
 }
 
 static void test_signed_short_round_up_generic(void) {
+    TEST_ASSERT_EQ(signed_short_round_up_generic(-5, 3), -3, "Round -5 up to nearest multiple of 3");
     TEST_ASSERT_EQ(signed_short_round_up_generic(8, 2), 8, "Round 8 up to nearest multiple of 2: Roundto is power of 2");
     TEST_ASSERT_EQ(signed_short_round_up_generic(5, 3), 6, "Round 5 up to nearest multiple of 3");
     TEST_ASSERT_EQ(signed_short_round_up_generic(12, 1), 12, "Round 12 up to nearest multiple of 1: no-op operation");
@@ -86,6 +87,7 @@ static void test_signed_short_round_up_generic(void) {
 }
 
 static void test_signed_short_round_down_generic(void) {
+    TEST_ASSERT_EQ(signed_short_round_down_generic(-5, 3), -10, "Round -5 down to nearest multiple of 3");
     TEST_ASSERT_EQ(signed_short_round_down_generic(19, 4), 16, "Round 19 down to nearest multiple of 4: Roundto is power of 2");
     TEST_ASSERT_EQ(signed_short_round_down_generic(5, 3), 3, "Round 5 down to nearest multiple of 3");
     TEST_ASSERT_EQ(signed_short_round_down_generic(12, 1), 12, "Round 12 down to nearest multiple of 1: no-op operation");
@@ -114,6 +116,7 @@ static void test_unsigned_short_round_down_generic(void) {
 }
 
 static void test_signed_int_round_up_generic(void) {
+    TEST_ASSERT_EQ(signed_int_round_up_generic(-5, 3), -3, "Round -5 up to nearest multiple of 3");
     TEST_ASSERT_EQ(signed_int_round_up_generic(8, 2), 8, "Round 8 up to nearest multiple of 2: Roundto is power of 2");
     TEST_ASSERT_EQ(signed_int_round_up_generic(5, 3), 6, "Round 5 up to nearest multiple of 3");
     TEST_ASSERT_EQ(signed_int_round_up_generic(12, 1), 12, "Round 12 up to nearest multiple of 1: no-op operation");
@@ -123,6 +126,7 @@ static void test_signed_int_round_up_generic(void) {
 }
 
 static void test_signed_int_round_down_generic(void) {
+    TEST_ASSERT_EQ(signed_int_round_down_generic(-5, 3), -10, "Round -5 down to nearest multiple of 3");
     TEST_ASSERT_EQ(signed_int_round_down_generic(19, 4), 16, "Round 19 down to nearest multiple of 4: Roundto is power of 2");
     TEST_ASSERT_EQ(signed_int_round_down_generic(5, 3), 3, "Round 5 down to nearest multiple of 3");
     TEST_ASSERT_EQ(signed_int_round_down_generic(12, 1), 12, "Round 12 down to nearest multiple of 1: no-op operation");
