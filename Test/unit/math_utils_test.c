231f93d4bfd082e4d6921cfdbd3646770a22639e
seagate <pratiksha.r.hire@seagate.com>
2026-04-02 14:11:03 +0530

    Made changes to function test_raise_to_power

--- Test/unit/math_utils_test.c
+++ Test/unit/math_utils_test.c
@@ -888,6 +888,7 @@ static void test_raise_to_power(void) {
     TEST_ASSERT(fabs(raise_to_power(2, -4) - 0.0625) < EPSILON, "2 raised to power -4");
     TEST_ASSERT(fabs(raise_to_power(3, -2) - 0.11111) < EPSILON, "3 raised to power -2");
     TEST_ASSERT(isnan(raise_to_power(0, -4)), "0 raised to power -4 returns NaN");
+    TEST_ASSERT(fabs(raise_to_power(4, 0) - 1) < EPSILON, "4 to power 0 is 1");
 }
 
 void run_math_utils_tests(void) {
