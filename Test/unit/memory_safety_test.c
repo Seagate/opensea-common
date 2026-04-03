1ec270b6fe55ed6e477222dbf97910b41eb80043
seagate <pratiksha.r.hire@seagate.com>
2026-04-03 10:19:26 +0530

    Made changes to function test_realloc_aligned

--- Test/unit/memory_safety_test.c
+++ Test/unit/memory_safety_test.c
@@ -439,9 +439,9 @@ static void test_realloc_aligned(void) {
     }
 
     // Test when size is zero, should free the original block and return null
-    int* ptr2 = realloc_aligned(new_ptr, element_size * new_num_elements, 0, alignment);
-    TEST_ASSERT(ptr2 == NULL, "realloc_aligned should return a null pointer when reallocating to zero");
-    
+    int* new_ptr = realloc_aligned(new_ptr, element_size * new_num_elements, 0, alignment);
+    TEST_ASSERT(new_ptr == NULL, "realloc_aligned should return a null pointer when reallocating to zero");
+
     free_aligned(new_ptr);
 }
 
