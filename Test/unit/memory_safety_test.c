6623e199a8770364485b6a46704a574654c732dd
seagate <pratiksha.r.hire@seagate.com>
2026-04-06 13:55:45 +0530

    Testing function test_safe_memcmove in memory_safety.h

--- Test/unit/memory_safety_test.c
+++ Test/unit/memory_safety_test.c
@@ -885,6 +885,7 @@ void run_memory_safety_tests(void) {
     test_safe_memcpy();
     test_safe_memcpy_no_overlap();
     test_safe_memccpy();
+    test_safe_memcmove();
     test_get_memalignment();
     test_SIZE_OF_STACK_ARRAY();
     #if defined(POSIX_1990) || defined(BSD4_2)
