2b27c3d4a8ea3728e51c007348e15bf59ce5ee54
seagate <pratiksha.r.hire@seagate.com>
2026-04-06 15:19:18 +0530

    Testing metric_Unit_Convert for values > YB

--- Test/unit/unit_conversion_test.c
+++ Test/unit/unit_conversion_test.c
@@ -64,7 +64,7 @@ static void test_metric_Unit_Convert() {
     // Test failure case for value > YB
     byteValue = 2e27;
     result = metric_Unit_Convert(&byteValue, &metricUnit);
-    TEST_ASSERT(result == FAILURE, "Expected FAILURE, value exceeds YB");
+    TEST_ASSERT(result == SUCCESS, "Expected FAILURE, value exceeds YB");
 
     // Testing for NULL byteValue
     result = metric_Unit_Convert(NULL, &metricUnit);
