74e4ce9384fd2ddf40e5ea663068a68da766675a
seagate <pratiksha.r.hire@seagate.com>
2026-04-08 11:51:13 +0530

    Made changes to function test_get_And_Validate_Integer_Input_UC

--- Test/unit/io_utils_test.c
+++ Test/unit/io_utils_test.c
@@ -179,6 +179,7 @@ static void test_get_And_Validate_Integer_Input_US(void) {
     TEST_ASSERT(!get_And_Validate_Integer_Input_US("xyz", NULL, ALLOW_UNIT_NONE, &outputInteger), "Could not convert string to integer successfully");
     TEST_ASSERT(!get_And_Validate_Integer_Input_US("123KB", NULL, ALLOW_UNIT_NONE, &outputInteger), "Could not convert string to integer successfully");
     TEST_ASSERT(!get_And_Validate_Integer_Input_US("12.5", NULL, ALLOW_UNIT_NONE, &outputInteger), "Could not convert string to integer successfully");
+}
 
 static void test_get_And_Validate_Integer_Input_UC(void) {
     unsigned char outputInteger;
