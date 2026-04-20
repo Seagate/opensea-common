71887a0a5720ab4821e91604859ba458faa23538
seagate <pratiksha.r.hire@seagate.com>
2026-04-20 10:03:57 +0530

    Debugging function test_safe_memset when destsz and count = 0

--- Test/unit/memory_safety_test.c
+++ Test/unit/memory_safety_test.c
@@ -266,8 +266,9 @@ static void test_safe_memset(void) {
         TEST_ASSERT(buffer[i] == '3', "safe_memset should set all bytes in the buffer to '3'");
     }
 
-    // Test when destsz = 0
+    // Test when destsz = 0 and count = 0
     TEST_ASSERT(safe_memset(buffer, 0, '3', 0) != 0, "safe_memset should return an error code when destsz is zero");
+    printf("errno: %d\n", errno);
 
     // Test when dest is NULL
     // TEST_ASSERT(safe_memset(NULL, sizeof(buffer), '3', sizeof(buffer)) != 0, "safe_memset should return an error code when dest is NULL");
