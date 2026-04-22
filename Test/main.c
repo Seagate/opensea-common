8a3343f4727d9065344804519932ba183ce8f8c3
seagate <pratiksha.r.hire@seagate.com>
2026-04-22 10:30:53 +0530

    Corrected function names in precision_timer_and_sleep_test.c

--- Test/main.c
+++ Test/main.c
@@ -3,15 +3,15 @@
 #include "framework/test_framework.h"
 
 // Forward declarations of test functions
-// void run_bit_manip_tests(void);
-// void run_math_utils_tests(void);
-// void run_string_utils_tests(void);
-// void run_io_utils_tests(void);
-// void run_memory_safety_tests(void);
-// void run_pattern_utils_tests(void);
-// void run_sort_and_search_tests(void);
-// void run_type_conversion_tests(void);
-// void run_unit_conversion_tests(void);
+void run_bit_manip_tests(void);
+void run_math_utils_tests(void);
+void run_string_utils_tests(void);
+void run_io_utils_tests(void);
+void run_memory_safety_tests(void);
+void run_pattern_utils_tests(void);
+void run_sort_and_search_tests(void);
+void run_type_conversion_tests(void);
+void run_unit_conversion_tests(void);
 void run_precision_timer_and_sleep_tests(void);
 
 int main(void)
@@ -19,18 +19,18 @@ int main(void)
     printf("Starting unit tests...\n\n");
 
     // set constraint handler to ignore errors to prevent test suite from crashing on expected failures
-    // set_Constraint_Handler(ERR_IGNORE);
+    set_Constraint_Handler(ERR_IGNORE);
 
     // Run individual test suites
-    // run_bit_manip_tests();
-    // run_math_utils_tests();
-    // run_string_utils_tests();
-    // run_io_utils_tests();
-    // run_memory_safety_tests();
-    // run_pattern_utils_tests();
-    // run_sort_and_search_tests();
-    // run_type_conversion_tests();
-    // run_unit_conversion_tests();
+    run_bit_manip_tests();
+    run_math_utils_tests();
+    run_string_utils_tests();
+    run_io_utils_tests();
+    run_memory_safety_tests();
+    run_pattern_utils_tests();
+    run_sort_and_search_tests();
+    run_type_conversion_tests();
+    run_unit_conversion_tests();
     run_precision_timer_and_sleep_tests();
     
     // Print summary of test results
--- Test/unit/precision_timer_and_sleep_test.c
+++ Test/unit/precision_timer_and_sleep_test.c
@@ -6,14 +6,14 @@
 DECLARE_SEATIMER(timer);
 
 // Creates and frees a seatimer on the heap to test the safe_free_seatimer function.
-static void create_free_seatimer_test(void) {
+static void test_create_free_seatimer(void) {
     NEW_SEATIMER(heapTimer);
     TEST_ASSERT(heapTimer != M_NULLPTR, "heapTimer should not be null after allocation");
     safe_free_seatimer(&heapTimer);
     TEST_ASSERT(heapTimer == M_NULLPTR, "heapTimer should be null after freeing");
 }
 
-static void get_Nano_Seconds_test(void) {
+static void test_get_Nano_Seconds(void) {
     start_Timer(&timer);
     sleepns(SLEEP_MICROSECONDS * 1000); // Convert microseconds to nanoseconds
     stop_Timer(&timer);
@@ -21,7 +21,7 @@ static void get_Nano_Seconds_test(void) {
     TEST_ASSERT(elapsedNanoSeconds >= (SLEEP_MICROSECONDS * 1000), "Elapsed nanoseconds should be at least the sleep time in nanoseconds");
 }
 
-static void get_Micro_Seconds_test(void) {
+static void test_get_Micro_Seconds(void) {
     start_Timer(&timer);
     sleepus(SLEEP_MICROSECONDS);
     stop_Timer(&timer);
@@ -29,7 +29,7 @@ static void get_Micro_Seconds_test(void) {
     TEST_ASSERT(elapsedMicroSeconds >= SLEEP_MICROSECONDS, "Elapsed microseconds should be at least the sleep time in microseconds");
 }
 
-static void get_Milli_Seconds_test(void) {
+static void test_get_Milli_Seconds(void) {
     start_Timer(&timer);
     sleepms(SLEEP_MICROSECONDS / 1000); // Convert microseconds to milliseconds
     stop_Timer(&timer);
@@ -37,7 +37,7 @@ static void get_Milli_Seconds_test(void) {
     TEST_ASSERT(elapsedMilliSeconds >= (SLEEP_MICROSECONDS / 1000.0), "Elapsed milliseconds should be at least the sleep time in milliseconds");
 }
 
-static void get_Seconds_test(void) {
+static void test_get_Seconds(void) {
     start_Timer(&timer);
     uint32_t threshold = UINT32_MAX / 1000;
     errno_t err = sleepsec(threshold + 1);
@@ -49,9 +49,9 @@ static void get_Seconds_test(void) {
 }
 
 void run_precision_timer_and_sleep_tests(void) {
-    create_free_seatimer_test();
-    get_Nano_Seconds_test();
-    get_Micro_Seconds_test();
-    get_Milli_Seconds_test();
-    get_Seconds_test();
+    test_create_free_seatimer();
+    test_get_Nano_Seconds();
+    test_get_Micro_Seconds();
+    test_get_Milli_Seconds();
+    test_get_Seconds();
 }
