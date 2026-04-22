4c673fbdf9bf63e8fdf75510cc1993b5a0704d77
seagate <pratiksha.r.hire@seagate.com>
2026-04-22 09:52:52 +0530

    Added test function create_free_seatimer_test which Creates and frees a seatimer on the heap to test the safe_free_seatimer function

--- Test/unit/precision_timer_test.c
+++ Test/unit/precision_timer_test.c
@@ -1,6 +1,7 @@
 #include"../framework/test_framework.h"
 #include"../../include/precision_timer.h"
 #include"../testConstants.h"
+#include <unistd.h>
 
 DECLARE_SEATIMER(timer);
 
@@ -12,17 +13,6 @@ static void create_free_seatimer_test(void) {
     TEST_ASSERT(heapTimer == M_NULLPTR, "heapTimer should be null after freeing");
 }
 
-static void start_Timer_test(void) {
-    start_Timer(&timer);
-    TEST_ASSERT(timer.timerStart != UINT64_C(0), "timer start time should not be zero after starting the timer");
-}
-
-static void stop_Timer_test(void) {
-    stop_Timer(&timer);
-    TEST_ASSERT(timer.timerStop != UINT64_C(0), "timer stop time should not be zero after stopping the timer");
-}
-
 void run_precision_timer_tests(void) {
-    start_Timer_test();
-    stop_Timer_test();
+    create_free_seatimer_test();
 }
