5fcf1c1a3adf791d9d83586cbaec63edba3c4d42
seagate <pratiksha.r.hire@seagate.com>
2026-04-22 09:36:39 +0530

    Added test function start_Timer_test

--- Test/unit/precision_timer_test.c
+++ Test/unit/precision_timer_test.c
@@ -2,6 +2,13 @@
 #include"../../include/precision_timer.h"
 #include"../testConstants.h"
 
+DECLARE_SEATIMER(timer);
+
+static void start_Timer_test(void) {
+    start_Timer(&timer);
+    TEST_ASSERT(timer.timerStart != UINT64_C(0), "timer start time should not be zero after starting the timer");
+}
+
 void run_precision_timer_tests(void) {
-    printf("Running Precision Timer Tests...\n");
+    start_Timer_test();
 }
