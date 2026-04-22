92b53588dd30a5884a0f231b0b256eb2c839c562
seagate <pratiksha.r.hire@seagate.com>
2026-04-22 10:15:32 +0530

    Combined precision_timer.h and sleep.h tests

--- Test/unit/precision_timer_and_sleep_test.c
+++ Test/unit/precision_timer_and_sleep_test.c
@@ -1,7 +1,7 @@
 #include"../framework/test_framework.h"
 #include"../../include/precision_timer.h"
+#include"../../include/sleep.h"
 #include"../testConstants.h"
-#include <unistd.h>
 
 DECLARE_SEATIMER(timer);
 
@@ -15,7 +15,7 @@ static void create_free_seatimer_test(void) {
 
 static void get_Nano_Seconds_test(void) {
     start_Timer(&timer);
-    usleep(SLEEP_MICROSECONDS);
+    sleepns(SLEEP_MICROSECONDS * 1000); // Convert microseconds to nanoseconds
     stop_Timer(&timer);
     uint64_t elapsedNanoSeconds = get_Nano_Seconds(timer);
     TEST_ASSERT(elapsedNanoSeconds >= (SLEEP_MICROSECONDS * 1000), "Elapsed nanoseconds should be at least the sleep time in nanoseconds");
@@ -23,7 +23,7 @@ static void get_Nano_Seconds_test(void) {
 
 static void get_Micro_Seconds_test(void) {
     start_Timer(&timer);
-    usleep(SLEEP_MICROSECONDS);
+    sleepus(SLEEP_MICROSECONDS);
     stop_Timer(&timer);
     double elapsedMicroSeconds = get_Micro_Seconds(timer);
     TEST_ASSERT(elapsedMicroSeconds >= SLEEP_MICROSECONDS, "Elapsed microseconds should be at least the sleep time in microseconds");
@@ -31,7 +31,7 @@ static void get_Micro_Seconds_test(void) {
 
 static void get_Milli_Seconds_test(void) {
     start_Timer(&timer);
-    usleep(SLEEP_MICROSECONDS);
+    sleepms(SLEEP_MICROSECONDS / 1000); // Convert microseconds to milliseconds
     stop_Timer(&timer);
     double elapsedMilliSeconds = get_Milli_Seconds(timer);
     TEST_ASSERT(elapsedMilliSeconds >= (SLEEP_MICROSECONDS / 1000.0), "Elapsed milliseconds should be at least the sleep time in milliseconds");
@@ -39,13 +39,13 @@ static void get_Milli_Seconds_test(void) {
 
 static void get_Seconds_test(void) {
     start_Timer(&timer);
-    usleep(SLEEP_MICROSECONDS);
+    sleepsec(SLEEP_MICROSECONDS / 1000000); // Convert microseconds to seconds
     stop_Timer(&timer);
     double elapsedSeconds = get_Seconds(timer);
     TEST_ASSERT(elapsedSeconds >= (SLEEP_MICROSECONDS / 1000000.0), "Elapsed seconds should be at least the sleep time in seconds");
 }
 
-void run_precision_timer_tests(void) {
+void run_precision_timer_and_sleep_tests(void) {
     create_free_seatimer_test();
     get_Nano_Seconds_test();
     get_Micro_Seconds_test();
