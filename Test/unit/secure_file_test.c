53ee5ec977d36959e864fd87a12b4882d6f2ea87
seagate <pratiksha.r.hire@seagate.com>
2026-04-23 10:24:16 +0530

    Debugging test function test_os_Get_File_Unique_Identifying_Information

--- Test/unit/secure_file_test.c
+++ Test/unit/secure_file_test.c
@@ -34,8 +34,18 @@ static void test_os_Get_File_Unique_Identifying_Information(void) {
     fileUniqueIDInfo *id1 = os_Get_File_Unique_Identifying_Information(f);
     fileUniqueIDInfo *id2 = os_Get_File_Unique_Identifying_Information(f);
 
-    printf("ID1: inode=%" PRIu64 ", deviceid=%" PRIu64 "\n", id1->inode, id1->deviceid);
-    printf("ID2: inode=%" PRIu64 ", deviceid=%" PRIu64 "\n", id2->inode, id2->deviceid);
+    if (!id1 || !id2) {
+        fprintf(stderr, "NULL ID detected\n");
+        fclose(f);
+        return;
+    }
+
+    fprintf(stderr, "ID1: inode=%" PRIu64 ", deviceid=%" PRIu64 "\n",
+    id1->inode, id1->deviceid);
+
+    fprintf(stderr, "ID2: inode=%" PRIu64 ", deviceid=%" PRIu64 "\n",
+    id2->inode, id2->deviceid);
+    
     TEST_ASSERT(compare_File_Unique_ID(id1, id2), "File unique IDs should be equal");
 
     // Test for different files
