783e326de3b256be2297ec2f5244278451fc8b7a
seagate <pratiksha.r.hire@seagate.com>
2026-04-23 10:07:45 +0530

    Added test function test_compare_File_Unique_ID

--- Test/unit/secure_file_test.c
+++ Test/unit/secure_file_test.c
@@ -2,6 +2,28 @@
 #include"../../include/secure_file.h"
 #include"../testConstants.h"
 
-void run_secure_file_tests(void) {
-    printf("Running secure file tests...\n");
+static void test_compare_File_Unique_ID(void) {
+    fileUniqueIDInfo a = {0}, b = {0};
+
+    // The inode and deviceid must be equal for the unique IDs to be considered equal.
+    a.inode = 100;
+    a.deviceid = 200;
+
+    b.inode = 100;
+    b.deviceid = 200;
+
+    TEST_ASSERT(compare_File_Unique_ID(&a, &b), "File unique IDs should be equal");
+
+    // Test for different inodes
+    b.inode = 101;
+    TEST_ASSERT(!compare_File_Unique_ID(&a, &b), "File unique IDs should not be equal when inodes differ");
+
+    // Test for different device IDs
+    b.inode = 100;
+    b.deviceid = 201;
+    TEST_ASSERT(!compare_File_Unique_ID(&a, &b), "File unique IDs should not be equal when device IDs differ");
 }
+
+void run_secure_file_tests(void) {
+    test_compare_File_Unique_ID();
+} 
