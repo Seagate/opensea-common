06268fe9ab3ead2a46cb84d4d1042443cf7fbc3c
seagate <pratiksha.r.hire@seagate.com>
2026-05-18 12:27:15 +0530

    fixing function test_secure_SetPos_File

--- Test/unit/secure_file_test.c
+++ Test/unit/secure_file_test.c
@@ -774,9 +774,9 @@ static void test_secure_SetPos_File(void) {
 
     // Test when file is closed
     fclose(fileInfo4->file);
-    fileInfo4->file = NULL;
     result = secure_SetPos_File(fileInfo4, &pos);
     TEST_ASSERT(result == SEC_FILE_FAILURE, "secure_SetPos_File should return failure when file is closed");
+    fileInfo4->file = NULL;
     free_Secure_File_Info(&fileInfo4);
 }
 
