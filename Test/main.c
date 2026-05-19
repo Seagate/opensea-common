473d2fa9969f286b2153ddd9e12778e891cdb60b
seagate <pratiksha.r.hire@seagate.com>
2026-05-19 12:16:13 +0530

    Testing file string_utils.h for windows

--- Test/main.c
+++ Test/main.c
@@ -5,7 +5,7 @@
 // Forward declarations of test functions
 void run_bit_manip_tests(void);
 void run_math_utils_tests(void);
-// void run_string_utils_tests(void);
+void run_string_utils_tests(void);
 // void run_io_utils_tests(void);
 // void run_memory_safety_tests(void);
 // void run_pattern_utils_tests(void);
@@ -27,7 +27,7 @@ int main(void)
     // Run individual test suites
     run_bit_manip_tests();
     run_math_utils_tests();
-    // run_string_utils_tests();
+    run_string_utils_tests();
     // run_io_utils_tests();
     // run_memory_safety_tests();
     // run_pattern_utils_tests();
