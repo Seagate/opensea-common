2b2b2dd7538db7c69e78b4cfd86394f5d6d4522b
seagate <pratiksha.r.hire@seagate.com>
2026-05-19 11:46:03 +0530

    Testing file bit_manip.h for windows

--- Test/main.c
+++ Test/main.c
@@ -4,18 +4,18 @@
 
 // Forward declarations of test functions
 void run_bit_manip_tests(void);
-void run_math_utils_tests(void);
-void run_string_utils_tests(void);
-void run_io_utils_tests(void);
-void run_memory_safety_tests(void);
-void run_pattern_utils_tests(void);
-void run_sort_and_search_tests(void);
-void run_type_conversion_tests(void);
-void run_unit_conversion_tests(void);
-void run_precision_timer_and_sleep_tests(void);
-#if defined(__linux__)
-void run_secure_file_tests(void);
-#endif
+// void run_math_utils_tests(void);
+// void run_string_utils_tests(void);
+// void run_io_utils_tests(void);
+// void run_memory_safety_tests(void);
+// void run_pattern_utils_tests(void);
+// void run_sort_and_search_tests(void);
+// void run_type_conversion_tests(void);
+// void run_unit_conversion_tests(void);
+// void run_precision_timer_and_sleep_tests(void);
+// #if defined(__linux__)
+// void run_secure_file_tests(void);
+// #endif
 
 int main(void)
 {
@@ -26,18 +26,18 @@ int main(void)
 
     // Run individual test suites
     run_bit_manip_tests();
-    run_math_utils_tests();
-    run_string_utils_tests();
-    run_io_utils_tests();
-    run_memory_safety_tests();
-    run_pattern_utils_tests();
-    run_sort_and_search_tests();
-    run_type_conversion_tests();
-    run_unit_conversion_tests();
-    run_precision_timer_and_sleep_tests();
-    #if defined(__linux__)
-    run_secure_file_tests();
-    #endif
+    // run_math_utils_tests();
+    // run_string_utils_tests();
+    // run_io_utils_tests();
+    // run_memory_safety_tests();
+    // run_pattern_utils_tests();
+    // run_sort_and_search_tests();
+    // run_type_conversion_tests();
+    // run_unit_conversion_tests();
+    // run_precision_timer_and_sleep_tests();
+    // #if defined(__linux__)
+    // run_secure_file_tests();
+    // #endif
     
     // Print summary of test results
     print_test_summary();
