cd83fca27c4e0fd6871c0f3e2eaf6c508e822c39
seagate <pratiksha.r.hire@seagate.com>
2026-06-02 13:48:34 +0530

    Debugging io_utils_test.c for Windows MSVC

--- Test/unit/io_utils_test.c
+++ Test/unit/io_utils_test.c
@@ -1775,5 +1775,5 @@ void run_io_utils_tests(void) {
     // test_safe_atof();
     // test_checked_fputs();
     // test_print_str();
-    println("Running io_utils tests!");
+    printf("Running io_utils tests!\n");
 }
