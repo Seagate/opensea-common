86d3c8080cc2efb5cce4ce44ec259534068b874f
seagate <pratiksha.r.hire@seagate.com>
2026-06-02 15:09:31 +0530

    Testing function test_getline for Windows MSVC

--- Test/unit/io_utils_test.c
+++ Test/unit/io_utils_test.c
@@ -1742,7 +1742,7 @@ void run_io_utils_tests(void) {
     test_get_And_Validate_Double_Input();
     test_get_And_Validate_LDouble_Input();
     test_get_Valid_Integer_Input();
-    // test_getline();
+    test_getline();
     // test_getdelim();
     // test_asprintf();
     // test_vasprintf();
