2501b9cb81cc498bb0d83e515cdb20d587f48b4b
seagate <pratiksha.r.hire@seagate.com>
2026-06-03 15:07:11 +0530

    Testing function test_safe_freopen

--- Test/unit/io_utils_test.c
+++ Test/unit/io_utils_test.c
@@ -1766,7 +1766,7 @@ void run_io_utils_tests(void) {
     test_safe_fopen();
     test_safe_freopen();
     test_safe_tmpfile();
-    test_safe_gets();
+    // test_safe_gets();
     // test_safe_strtol();
     // test_safe_strtoll();
     // test_safe_strtoul();
