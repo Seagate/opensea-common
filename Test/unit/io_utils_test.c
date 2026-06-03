c915955874b70cf50a16a8c851b666709ff14e04
seagate <pratiksha.r.hire@seagate.com>
2026-06-03 13:17:17 +0530

    Testing function test_safe_freopen for Windows MSVC

--- Test/unit/io_utils_test.c
+++ Test/unit/io_utils_test.c
@@ -1757,7 +1757,7 @@ void run_io_utils_tests(void) {
     test_flush_stdout();
     test_flush_stderr();
     test_safe_fopen();
-    // test_safe_freopen();
+    test_safe_freopen();
     // test_safe_tmpfile();
     // test_safe_gets();
     // test_safe_strtol();
