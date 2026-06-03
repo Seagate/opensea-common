7f97aa5d65e3171d0215813f7f1790f4e5f5177b
seagate <pratiksha.r.hire@seagate.com>
2026-06-03 13:06:47 +0530

    Testing function test_print_Return_Enum for Windows MSVC

--- Test/unit/io_utils_test.c
+++ Test/unit/io_utils_test.c
@@ -1753,7 +1753,7 @@ void run_io_utils_tests(void) {
     // test_set_Console_Colors(); Skip for now as it is complicated to test
     test_print_Data_Buffer();
     test_print_Pipe_Data();
-    // test_print_Return_Enum();
+    test_print_Return_Enum();
     // test_flush_stdout();
     // test_flush_stderr();
     // test_safe_fopen();
