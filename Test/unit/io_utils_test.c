476be57ee38531d8ab48054a72a4527cb0080918
seagate <pratiksha.r.hire@seagate.com>
2026-06-03 13:12:24 +0530

    Testing function test_flush_stderr for Windows MSVC

--- Test/unit/io_utils_test.c
+++ Test/unit/io_utils_test.c
@@ -1755,7 +1755,7 @@ void run_io_utils_tests(void) {
     test_print_Pipe_Data();
     test_print_Return_Enum();
     test_flush_stdout();
-    // test_flush_stderr();
+    test_flush_stderr();
     // test_safe_fopen();
     // test_safe_freopen();
     // test_safe_tmpfile();
