5d10739eb43d114d7e64a1b338f5637c63129949
seagate <pratiksha.r.hire@seagate.com>
2026-06-04 09:58:17 +0530

    Testing function test_safe_strtoimax

--- Test/unit/io_utils_test.c
+++ Test/unit/io_utils_test.c
@@ -1782,7 +1782,7 @@ void run_io_utils_tests(void) {
     test_safe_strtoll();
     test_safe_strtoul();
     test_safe_strtoull();
-    // test_safe_strtoimax();
+    test_safe_strtoimax();
     // test_safe_strtoumax();
     // test_safe_strtof();
     // test_safe_strtod();
