93b3c05a4b8730e9693593aefb2f7afd72500bc0
seagate <pratiksha.r.hire@seagate.com>
2026-06-04 10:10:35 +0530

    Testing function test_safe_strtold

--- Test/unit/io_utils_test.c
+++ Test/unit/io_utils_test.c
@@ -1786,7 +1786,7 @@ void run_io_utils_tests(void) {
     test_safe_strtoumax();
     test_safe_strtof();
     test_safe_strtod();
-    // test_safe_strtold();
+    test_safe_strtold();
     // test_safe_atoi();
     // test_safe_atol();
     // test_safe_atoll();
