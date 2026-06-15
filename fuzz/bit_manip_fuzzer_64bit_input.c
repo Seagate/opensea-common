0653cb4239570535e179046ce5b8a55e6a782d65
seagate <pratiksha.r.hire@seagate.com>
2026-06-15 09:56:15 +0530

    Added functions get_Word0_uint64, get_Word1_uint64, get_Word2_uint64 and get_Word3_uint64 to bit_manip_fuzzer_64bit_input

--- fuzz/bit_manip_fuzzer_64bit_input.c
+++ fuzz/bit_manip_fuzzer_64bit_input.c
@@ -12,10 +12,21 @@ int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
         value |= ((uint64_t)data[i]) << (8 * i);
     }
 
-    uint32_t lower_32bit = get_DWord0(value);
-    uint32_t upper_32bit = get_DWord1(value);
+    uint32_t lower_uint32bit = get_DWord0(value);
+    uint32_t upper_uint32bit = get_DWord1(value);
+    int32_t lower_int32bit = M_DoubleWordInt0(value);
+    int32_t upper_int32bit = M_DoubleWordInt1(value);
+    uint16_t lower_uint16bit = get_Word0_uint64(value);
+    uint16_t second_lower_uint16bit = get_Word1_uint64(value);
+    uint16_t second_highest_uint16bit = get_Word2_uint64(value);
+    uint16_t highest_uint16bit = get_Word3_uint64(value);
 
-    if (lower_32bit != (uint32_t)value || upper_32bit != (uint32_t)(value >> 32)) {
+    if (lower_uint32bit != (uint32_t)value || upper_uint32bit != (uint32_t)(value >> 32) ||
+    lower_int32bit != (int32_t)value || upper_int32bit != (int32_t)(value >> 32) ||
+    lower_uint16bit != (value & UINT64_C(0x000000000000FFFF)) || second_lower_uint16bit !=
+    ((value & UINT64_C(0x00000000FFFF0000)) >> 16) || second_highest_uint16bit !=
+    ((value & UINT64_C(0x0000FFFF00000000)) >> 32) || highest_uint16bit !=
+    ((value & UINT64_C(0xFFFF000000000000)) >> 48)) {
         __builtin_trap();
     }
 
