33a2943f2150e8f771a38cc8afb712550f8eb8b0
seagate <pratiksha.r.hire@seagate.com>
2026-06-17 15:23:13 +0530

    Added fuzzing and validation for functions n_swap_8 and nibble_Swap in bit_manip_fuzzer_8bit_input

--- fuzz/bit_manip/bit_manip_fuzzer_8bit_input.c
+++ fuzz/bit_manip/bit_manip_fuzzer_8bit_input.c
@@ -39,5 +39,12 @@ int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
     uint64_t bytes_To_Uint64_result = bytes_To_Uint64(msb64, byte6_64, byte5_64, byte4_64, byte3_64, byte2_64, byte1_64, lsb64);
     if (bytes_To_Uint64_result != ((M_STATIC_CAST(uint64_t, msb64) << 56) | (M_STATIC_CAST(uint64_t, byte6_64) << 48) | (M_STATIC_CAST(uint64_t, byte5_64) << 40) | (M_STATIC_CAST(uint64_t, byte4_64) << 32) | (M_STATIC_CAST(uint64_t, byte3_64) << 24) | (M_STATIC_CAST(uint64_t, byte2_64) << 16) | (M_STATIC_CAST(uint64_t, byte1_64) << 8) | (M_STATIC_CAST(uint64_t, lsb64) << 0))) __builtin_trap();
 
+    uint8_t n_swap_8_result = n_swap_8(byte1_32);
+    if (n_swap_8_result != ((M_STATIC_CAST(uint8_t, ((byte1_32 & UINT8_C(0x0F)) << 4))) | (M_STATIC_CAST(uint8_t, ((byte1_32 & UINT8_C(0xF0)) >> 4))))) __builtin_trap();
+
+    uint8_t nibble_Swap_result = byte1_32;
+    nibble_Swap(&nibble_Swap_result);
+    if (nibble_Swap_result != n_swap_8_result) __builtin_trap();
+
     return 0;
 }
